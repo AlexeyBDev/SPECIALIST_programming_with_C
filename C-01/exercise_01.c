13f180614f91888f2a50eac6b93811f247bfb058
AlexeyBDev <jobabb82@yandex.ru>
2017-02-05 21:09:30 +0300

    delete cpp file

---
+++ C-01/exercise_01.c
@@ -0,0 +1,16 @@
+/* Вычислить среднее арифметическое двух значений х1 и х2.
+*
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	float x1, x2;
+	x1 = x2 = 0.0f;
+	printf("Enter 2 int value: ");
+	scanf("%f %f", &x1, &x2);
+
+	printf("The average of the numbers %.3f and %.3f is equal to: %.3f\n\n", x1, x2, (x1 + x2) / 2);
+	return 0;
+}
