6ac14efe08272ff1d391d3ac7cb4bc319542f3e9
AlexeyBDev <jobabb82@yandex.ru>
2017-02-05 20:47:10 +0300

    Add project files.

---
+++ C-02/exercise_01.c
@@ -0,0 +1,17 @@
+///* Вычислить среднее арифметическое двух значений х1 и х2.
+//*
+//*/
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x1, x2;
+//	x1 = x2 = 0;
+//
+//	printf("Enter 2 int value: ");
+//	scanf("%d %d", &x1, &x2);
+//
+//	printf("Еhe average of the numbers %d and %d is equal to: %d\n", x1, x2, (x1 + x2) / 2);
+//	return 0;
+//}
