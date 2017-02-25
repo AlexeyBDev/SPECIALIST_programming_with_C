be9c0481069991f7e3842e7adb982fd251b304a9
Admin <Admin@BS-STUDENT>
2017-02-25 17:21:42 +0300

    end 4 lesson

--- C-03/exercise_06_05.c
+++ C-03/exercise_06_05.c
@@ -5,44 +5,46 @@
 	c. поиска максимума
  */
 
-#include <stdio.h>
-#include <locale.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <time.h>
-#include "bubnov_function.h"
-
-
-
-#define SIZE_M 21U
- 
-int main()
-{
-	setlocale(LC_CTYPE, "rus");
-	
-	int *max;
-	int p = 0;
-		//max = &p;
-	int m[SIZE_M];
-	generatorArray(m, SIZE_M, 100);				// генерация значений массива
-	printArray(m);								// вывод значений массива
-
-	printf("Элемент с максимальным значением %d имеет номер %d\n", maxElementArray(m, &p), p);
-	printf("Элемент с минимальным значением %d имеет номер %d\n", minElementArray(m, &p), p);
-	sortArray(m);								// сортировка массива
-	printArray(m);								// вывод сортированного массива
-
-	DWORD nStartValue = time(NULL);
-	srand(nStartValue);
-
-	int element = rand() % (SIZE_M);
-	printf("Сгенерирован элемент - %d\n", element);
-	if (searchElementArray(element, m, &p)) {
-		printf("Элемент %d содержится в данном массиве под номером %d.\n", element, p);
-	}
-	else
-	{
-		printf("Элемент %d отсутствует в данном массиве.\n", element);
-	}
-	return 0;
-}
+//#include <stdio.h>
+//#include <locale.h>
+//#include <stdlib.h>
+//#include <windows.h>
+//#include <time.h>
+//#include "bubnov_function.h"
+//
+//
+//
+//#define SIZE_M 1000
+// 
+//int main()
+//{
+//	setlocale(LC_CTYPE, "rus");
+//	
+//	//size_t a; - данные тип гарантирует что им можно измерить любой объект созданные тут
+//	// size_t - тип для размеров
+//	int *max;
+//	int p = 0;
+//		//max = &p;
+//	int m[SIZE_M];
+//	generatorArray(m, SIZE_M, 100);				// генерация значений массива
+//	printArray(m);								// вывод значений массива
+//
+//	printf("Элемент с максимальным значением %d имеет номер %d\n", maxElementArray(m, &p), p);
+//	printf("Элемент с минимальным значением %d имеет номер %d\n", minElementArray(m, &p), p);
+//	sortArray(m);								// сортировка массива
+//	printArray(m);								// вывод сортированного массива
+//
+//	DWORD nStartValue = time(NULL);
+//	srand(nStartValue);
+//
+//	int element = rand() % (SIZE_M);
+//	printf("Сгенерирован элемент - %d\n", element);
+//	if (searchElementArray(element, m, &p)) {
+//		printf("Элемент %d содержится в данном массиве под номером %d.\n", element, p);
+//	}
+//	else
+//	{
+//		printf("Элемент %d отсутствует в данном массиве.\n", element);
+//	}
+//	return 0;
+//}
