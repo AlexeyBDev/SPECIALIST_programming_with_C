71acb63c3ebccc7b9f03e73d3eaebce78ffeeff1
Admin <Admin@BS-STUDENT>
2017-03-04 17:21:13 +0300

    end lesson 5

---
+++ C-05/inClassRoom_05.c
@@ -0,0 +1,18 @@
+// разобраться с многомерными массивами
+// внутренние таблици компилятора - что это?
+// 
+
+#include <stdio.h>
+#include <locale.h>
+
+//int a;
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+
+	int a = 5;
+	int *p = NULL;
+
+	return 0;
+}
